882d7382551a92c41a8db6059a65f169a314e435
Simão Gama <simaogama@MacBook-Pro-2.local>
2023-05-15 19:01:29 +0100

    added more forms

--- models/File.model.js
+++ models/File.model.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const fileSchema = new Schema({
   name: String,
+  tag: [String],
   month: String,
   fileClient: [{ type: Schema.Types.ObjectId, ref: 'Client' }],
   fileUrl: String
